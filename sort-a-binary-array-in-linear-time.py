abf36932cb20a41a1ecc967d010d637123673fad
Abhishek Dixit <47380596+abhid001@users.noreply.github.com>
2022-08-22 18:58:36 +0530

    Create sort-a-binary-array-in-linear-time.py

---
+++ sort-a-binary-array-in-linear-time.py
@@ -0,0 +1,10 @@
+'''
+Input : 1 0 0 1 0 1 0 1 1 1 1 1 1 0 0 1 1 0 1 0 0 
+Output : 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1
+Explanation: The output is a sorted array of 0 and 1
+
+Input : 1 0 1 0 1 0 1 0 
+Output : 0 0 0 0 1 1 1 1
+Explanation: The output is a sorted array of 0 and 1
+'''
+
